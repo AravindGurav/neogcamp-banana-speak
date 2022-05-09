09e27bf61c5012c48c7d9842146039f1fc75473f
AravindGurav <96325892+AravindGurav@users.noreply.github.com>
2022-05-09 15:08:09 +0530

    input output processing

--- app.js
+++ app.js
@@ -3,10 +3,18 @@
 
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+
+// outputDiv.innerText = "Aravind Gurav"
+
+// function buttonClick(){
+//     console.log("clicked!!");
+//     console.log(txtInput.value);    
+// }
 
 function buttonClick(){
-    console.log("clicked!!");
-    console.log(txtInput.value);    
+    outputDiv.innerText="asjasjasjasjasj "+ txtInput.value;
 }
+
 
 btnTranslate.addEventListener("click", buttonClick);
